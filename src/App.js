53fee78e9ff3e9f5a137f73374b91621b8916953
Osama Najjar <osama.najjar.m@gmail.com>
2018-09-17 23:29:53 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Header from "./components/Header";
+import "./style.css";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
