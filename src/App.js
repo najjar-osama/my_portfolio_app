d9ab13d8bff91baebaf967858f8b46b8d6405c0e
Osama Najjar <osama.najjar.m@gmail.com>
2018-09-18 00:16:05 +0200

    created nav bar comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Intro from "./components/Intro";
+import Nav from "./components/Nav";
 import "./style.css";
 class App extends Component {
   render() {
     return (
       <div className="loaded">
+        <Nav />
         <Intro />
       </div>
     );
