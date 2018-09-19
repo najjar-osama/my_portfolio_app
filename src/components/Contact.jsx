bb3392fd6555018e4cddd8468f0b7d65af4c0783
Osama Najjar <osama.najjar.m@gmail.com>
2018-09-20 01:30:53 +0200

    add the final touches

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,10 +5,10 @@ import linkedin from "../images/in.png";
 import twitter from "../images/twitter.png";
 
 const contactLinks = [
-  { link: "", icon: email },
-  { icon: github, link: " " },
-  { icon: linkedin, link: "" },
-  { icon: twitter, link: "" }
+  { icon: email, href: "mailto:osama.najjar.m@gmail.com" },
+  { icon: github, href: "https://github.com/najjar-osama/" },
+  { icon: linkedin, href: "https://www.linkedin.com/in/osama-najjar/" },
+  { icon: twitter, href: "https://twitter.com/NajjarOsama93" }
 ];
 const Contact = props => (
   <div className="contact-wrapper">
@@ -21,6 +21,9 @@ const Contact = props => (
             <img src={link.icon} alt={link.href} />
           </a>
         ))}
+      </div>
+      <div className="cp flex flex-content-spacing--center end-line">
+        <p>{`Osama Mohammad Najjar © ${new Date().getFullYear()}`}</p>
       </div>
     </div>
   </div>
